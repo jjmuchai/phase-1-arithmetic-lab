6f634552eb43a11ac054289cdca938c471f9c219
jjmuchai <jjmuchai151@gmail.com>
2025-03-23 10:20:59 +0300

    My updates to the project

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-
 // Write your code here
+
+// Multiplying two numbers to get 62
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // 31 * 2 = 62
+
+console.log(multiply); // Output: 62
+
+
+// Generate a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1;
+
+console.log(random); // Output: A random number between 1 and 100
+
+
+
+// Calculate the remainder when num3 is divided by num4
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4; // 10 % 6 = 4
+
+console.log(mod); // Output: 4
+
+
+
+// Find the highest number in a given set
+let max = Math.max(5, 10, 15, 20);
+
+console.log(max); // Output: 20
